457bf7e1f202195045856a9f76df6905c8e54000
Anders Jansson <anjansso@microsoft.com>
2021-03-28 22:32:23 +0200

    Update ch1

--- hub-and-spoke-vnet.tf
+++ hub-and-spoke-vnet.tf
@@ -58,7 +58,7 @@ resource "azurerm_subnet" "hub-azurefirewall-subnet" {
   name                 = "AzureFirewallSubnet"
   resource_group_name  = azurerm_resource_group.hub-spoke-microhack-rg.name
   virtual_network_name = azurerm_virtual_network.hub-vnet.name
-  address_prefix       = "10.0.4.0/27"
+  address_prefix       = "10.0.4.0/24"
 }
 resource "azurerm_subnet" "hub-dns" {
   name                 = "DNSSubnet"
